1cdc826cfdf4996481d67a3e5679f3d72147f035
Colleen Bove <colleenbove@gmail.com>
2021-09-13 10:44:08 -0400

    updated gls for Pathfinder that now works

--- code/Pathfinder_RasterCalculations.R
+++ code/Pathfinder_RasterCalculations.R
@@ -1,14 +1,14 @@
-###############################################################
-##### Pathfinder Raster Conversions and Slope Calculations ####
-###############################################################
-##--- Last edit was made by Colleen Bove on 15 March 2021 ---##
+###################################################################
+####### Pathfinder Raster Conversions and Slope Calculations ######
+###################################################################
+##--- Last edit was made by Colleen Bove on 13 September 2021 ---##
 
 #---- This script was used with the Pathfinder SST netCDF (pathfinder_combined_monthly_data.nc) to:
 #------- 1) add 'NA' to missing vaules (set as -52 in netCDF)
 #------- 2) convert all values from Kelvin to Celsius
 #------- 3) save edited raster as new netCDF (PathfinderSST_monthly_edit.nc)
 #------- 4) calculate slope of change in SST (C / decade) per pixel and save as .csv/.Rdata
-#------- 5) calculate p-value of SST slope per pixel and save as .csv
+#------- 5) calculate p-value of SST slope per pixel and save as .csv/.Rdata
 
 #--- This script was run on a high performance computing cluster due to time requirements (~ 3 hours)
 #--- You will need this script and the Pathfinder netCDF (pathfinder_combined_monthly_data.nc) on the cluster
@@ -24,6 +24,10 @@ Xmin<- -95
 Xmax<- -55
 Ymin<- 0
 Ymax<- 40
+
+## set the binning of p-values for plotting
+bins <- c(-Inf, 0.0001, 0.001, 0.01, 0.05, 0.1, Inf)
+bin_names <- c("< 0.0001", "0.0001 - 0.001", "0.001 - 0.01", "0.01 - 0.05", "0.05 - 0.1", "> 0.1")
 
 # libraryies needed for running code:
 library(ncdf4)
@@ -71,50 +75,57 @@ NAset <- function(x){x[x < -54]<-NA; return(x)} # Pathfinder uses the value -54 
 data_brick2 <- calc(data_brick2, NAset) # apply the NA function written above to raster
 data_brick2b <- calc(data_brick2, KtoC) # apply the Kelvin function written above to raster
 
+### Save the edited netCDF file and the slope raster/dataframe
+writeRaster(data_brick2b, filename = "PathfinderSST_monthly_edit_kelvin.nc", format="CDF", overwrite=TRUE)
+writeRaster(data_brick2, filename = "PathfinderSST_monthly_edit.nc", format="CDF", overwrite=TRUE)
+
+
+data_brick2 <- brick("PathfinderSST_monthly_edit.nc") # read SST data as rasterbrick
 
 ### Slope Calculation
 ## Calculate slope of temperature change across time for each pixel
 # gls function applied to the raster brick to calculate the slope of temperature change over timescale of raster (with account of temporal autocorrelation)
-gls.fun2 <- function(x, time){
-  if(all(is.na(x[1:200]))){ NA }
-  else{
+
+gls.fun2 <- function(x, time, na.rm=TRUE, ...){
+  if((sum(!is.na(x))) < 3){
+    return(NA)}
+  else {
     nlme::gls(x ~ time, correlation = corAR1(form = ~ 1 | time), na.action = na.omit, control = list(singular.ok = TRUE))$coefficients[2]*60*60*24 # slope here is degrees/day
-    #print(x)
-    #print(xyFromCell(raster, x))
   }}
 
 
-raster_slope2 <- calc(data_brick2b, function(x)lm.fun(x, time = time)) # function applied here
+raster_slope2 <- calc(data_brick2, fun = function(x){gls.fun2(x, time = time)}) 
 
 ## Create dataframe from calculated slope raster in C per decade for plotting
 raster_slope2 <- raster_slope2*(365.25*10) # this is currently C per decade
 path_slope <- as.data.frame(as(raster_slope2, "SpatialPixelsDataFrame")) # convert to a raster to da$
 colnames(path_slope) <- c("sst", "x", "y")
 
+## Save the slope raster:
+save(path_slope, file = "Pathfinder_slope.Rdata")
+write.csv(path_slope, file = "Pathfinder_slope.csv", row.names=FALSE)
+
+
 
 ### P value Calculation
 ## function to pull the p value per pixel of the simple lm applied to the full HadISST raster brick 
-pval.fun <- function(x, time){
-  if(any(is.na(x))){NA}
+pval.fun <- function(x, time, na.rm=TRUE, ...){
+  if((sum(!is.na(x))) < 3){
+    return(NA)}
   else{
     summary(nlme::gls(x ~ time, correlation = corAR1(form = ~ 1 | time), na.action = na.omit, control = list(singular.ok = TRUE)))$tTable[2,4] # pulls the p value
   }
 }
 
-
 ## function to pull the p value per pixel of the simple lm applied to the full Pathfinder raster brick 
-raster_pval2 <- calc(data_brick2b, function(x)pval.fun(x, time = time))
+raster_pval2 <- raster::calc(data_brick2, function(x){pval.fun(x, time = time)})
 
 # convert the raster to a df for plotting
-path_pavl <- as.data.frame(as(raster_pval2, "SpatialPixelsDataFrame")) # convert to a raster to dataframe
-colnames(path_pavl) <- c("pval", "x", "y")
-path_pavl$bins <- cut(path_pavl$pval, breaks = bins, labels = bin_names) # bins the pvales into 6 unique bins for plotting
+path_pval <- as.data.frame(as(raster_pval2, "SpatialPixelsDataFrame")) # convert to a raster to dataframe
+colnames(path_pval) <- c("pval", "x", "y")
+path_pval$bins <- cut(path_pval$pval, breaks = bins, labels = bin_names) # bins the pvales into 6 unique bins for plotting
 
 
-
-### Save the edited netCDF file and the slope raster/dataframe
-writeRaster(data_brick2b, filename = "PathfinderSST_monthly_edit.nc", format="CDF", overwrite=TRUE)
-save(path_slope, file = "Pathfinder_slope.Rdata")
-write.csv(path_slope, file = "Pathfinder_slope.csv", row.names=FALSE)
-write.csv(path_pavl, file = "Pathfinder_pval.csv", row.names=FALSE)
+save(path_pval, file = "Pathfinder_pval.Rdata")
+write.csv(path_pval, file = "Pathfinder_pval.csv", row.names=FALSE)
 
